fd2826ee39cc2035cf31ae6372f9a3efba650f8b
DimaGalchenko <galchenkodima31@gmail.com>
2024-07-27 08:09:20 +0300

    * implement query "Select users along with their favorite movies as array of identifiers"

---
+++ 3_select_user_favorite_movies.sql
@@ -0,0 +1,4 @@
+SELECT u.id as "ID", u.username as "Username", array_agg(m.id) From users u
+LEFT JOIN favorite_movies fm ON u.id = fm.user_id
+LEFT JOIN movies m ON u.id = m.id
+GROUP BY u.id, u.username;
