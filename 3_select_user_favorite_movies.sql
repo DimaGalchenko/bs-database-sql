bf0d57e31d66feacad4bdee87287df0b061b3bbe
DimaGalchenko <galchenkodima31@gmail.com>
2024-07-27 08:27:28 +0300

    *reformat

--- 3_select_user_favorite_movies.sql
+++ 3_select_user_favorite_movies.sql
@@ -1,4 +1,5 @@
-SELECT u.id as "ID", u.username as "Username", array_agg(m.id) From users u
-LEFT JOIN favorite_movies fm ON u.id = fm.user_id
-LEFT JOIN movies m ON u.id = m.id
+SELECT u.id as "ID", u.username as "Username", array_agg(m.id)
+From users u
+         LEFT JOIN favorite_movies fm ON u.id = fm.user_id
+         LEFT JOIN movies m ON u.id = m.id
 GROUP BY u.id, u.username;
